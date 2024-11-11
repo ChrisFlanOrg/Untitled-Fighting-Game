bdc275af5aea83aaf25092e59db520bf5ad57c58
Chris Flanagan <crflanag@gmail.com>
2024-11-10 19:25:31 -0800

    Animations more smooth

--- player.gd
+++ player.gd
@@ -3,19 +3,31 @@ extends CharacterBody2D
 @onready var anim_tree = $AnimationTree
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -800.0
+const JUMP_VELOCITY = -600.0
+
+var isJumping = false
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	elif is_on_floor() && isJumping:
+		isJumping = false
+		anim_tree["parameters/playback"].travel("Grounded")
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		isJumping = true
 		anim_tree.set("parameters/conditions/jump", true)
 	elif Input.is_action_just_pressed("Attack"):
 		anim_tree.set("parameters/conditions/attacking", true)
-		print("attack")
+		anim_tree["parameters/playback"].travel("Attack")
+		
+	if Input.is_action_just_pressed("ui_left"):
+		animated_sprite_2d.flip_h = true
+
+	elif Input.is_action_just_pressed("ui_right"):
+		animated_sprite_2d.flip_h = false
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
